2b17bc330135c7bb6c7fc3561de5e41a23ffb2c7
Chao <chyrsq2001@gmail.com>
2024-05-31 04:51:50 +0200

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,7 +8,7 @@ export const conexionDB = async () => {
     const url = `${connection.host}:${connection.port}`;
     console.log(colors.magenta.bold(`MongoDB Conectado en: ${url}`));
   } catch (error) {
-    console.log(colors.red.bold("Error al conectar a MongoDB"));
+    console.log(colors.red.bold(`Error al conectar a MongoDB por: ${error}`));
     exit(1);
   }
 };
